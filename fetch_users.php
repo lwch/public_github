5832593e3259e25951bf63a7302cb8a0cb608556
lwch <lwch748@gmail.com>
2015-09-24 00:30:48 +0800

    supported fetch_users

--- fetch_users.php
+++ fetch_users.php
@@ -1,3 +1,47 @@
 <?php
+require __DIR__.'/config.php';
 require __DIR__.'/lib/base.php';
-var_dump(curl_get('https://api.github.com/search/users?q=repos:%3E0&page=1'));
+
+date_default_timezone_set('America/Los_Angeles');
+
+$start = 0;
+$end   = time();
+$users = array();
+
+fetch($start, $end);
+
+function fetch($start, $end) {
+    global $users;
+    $s = date('Y-m-d\TH:i:sO', $start);
+    $e = date('Y-m-d\TH:i:sO', $end);
+    $save = $end;
+    $url = "https://api.github.com/search/users?q=created:$s..$e&sort=joined&access_token=".GITHUB_TOKEN;
+    $redo = false;
+    do {
+        list($status, $header, $body) = curl_get($url);
+        if ($status != 200) continue;
+        $body = json_decode($body, true);
+        if ($body['total_count'] > 1000) {
+            $end = ($start + $end) >> 1;
+            $e = date('Y-m-d\TH:i:sO', $end);
+            $url = "https://api.github.com/search/users?q=created:$s..$e&sort=joined&access_token=".GITHUB_TOKEN;
+            $redo = true;
+            echo 'redo: ', $s, '..', $e, "\n";
+            continue;
+        }
+        parse($body, $users);
+        $link = parse_link($header['Link']);
+        if (!isset($link['next'])) break;
+        $url = $link['next'];
+        echo $url, "\n";
+    } while (1);
+    echo 'count: ', count($users), "\n";
+    if ($redo) fetch($end, $save);
+}
+
+sort($users);
+var_dump($users);
+
+function parse($body, &$users) {
+    $users = array_merge($users, array_column($body['items'], 'login'));
+}
--- lib/base.php
+++ lib/base.php
@@ -6,6 +6,7 @@ function curl_get($url) {
     curl_setopt($curl, CURLOPT_HEADER, true);
     curl_setopt($curl, CURLOPT_USERAGENT, 'made from https://github.com/lwch/public_github');
     $ret = curl_exec($curl);
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     $header_len = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
     $header = array();
     foreach (explode("\r\n", substr($ret, 0, $header_len)) as $row) {
@@ -18,5 +19,22 @@ function curl_get($url) {
     }
     $body = substr($ret, $header_len);
     curl_close($curl);
-    return array('header' => $header, 'body' => $body);
+    return array($status, $header, $body);
+}
+function parse_link($link) {
+    $ret = array();
+    foreach (explode(',', $link) as $row) {
+        $row = trim($row);
+        $row = explode(';', $row, 2);
+        $row[0] = trim($row[0]);
+        $row[1] = trim($row[1]);
+        if (substr($row[0], 0, 1) == '<' and
+            substr($row[0], -1) == '>' and
+            substr($row[1], 0, 4) == 'rel=') {
+            $key = substr($row[1], 5, -1);
+            $val = substr($row[0], 1, -1);
+            $ret[$key] = $val;
+        }
+    }
+    return $ret;
 }
