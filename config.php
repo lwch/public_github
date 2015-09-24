b4fdba8628f165cdfb44c420a196a0052c2ba238
lwch <lwch748@gmail.com>
2015-09-24 10:40:40 -0400

    change config.php

--- config.php
+++ config.php
@@ -1,7 +1,2 @@
 <?php
-define('GITHUB_USER', 'xxxx');
-define('GITHUB_PASS', 'xxxx');
-
-# Application created from user=>Settings=>Applications
-define('GITHUB_CLIENT_ID', 'xxxx');
-define('GITHUB_CLIENT_SECRET', 'xxxx');
+define('GITHUB_TOKEN', 'xxxx'); # created from User=>Settings=>Personal access tokens
