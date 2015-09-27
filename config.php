44c0366d20ff5086ac90f723fd793f827fc3ee25
lwch <lwch748@gmail.com>
2015-09-27 03:31:37 -0400

    change config.php

--- config.php
+++ config.php
@@ -1,2 +1,5 @@
 <?php
 define('GITHUB_TOKEN', 'xxxx'); # created from User=>Settings=>Personal access tokens
+define('DB_NAME', 'public_github');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
