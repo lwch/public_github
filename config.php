53b28643c5f8fd489ea4e87b5b9d27a7da13493b
lwch <lwch748@gmail.com>
2015-10-05 09:46:50 -0400

    update config.php

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
-define('GITHUB_TOKEN', 'xxxx'); # created from User=>Settings=>Personal access tokens
+define('GITHUB_LIST_USERS_TOKEN', 'xxxx'); # created from User=>Settings=>Personal access tokens
+define('GITHUB_UPDATE_REPOS_TOKEN', 'xxxx'); # created from User=>Settings=>Personal access tokens
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'public_github');
 define('DB_USER', 'public_github');
 define('DB_PASS', '123456');
-
-define('KEEP_TASKQUEUE_LENGTH', true);
-define('TASKQUEUE_MAX_LENGTH',  1000);
