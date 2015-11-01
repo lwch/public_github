60f60ffe53ec658066f06dc12da2cf9d933e15dd
lwch <lwch748@gmail.com>
2015-11-01 23:40:50 +0800

    1. change algo for calc publish post, there are one or more posts to publish in an hour 2. add logs in task.push.repos.php

--- bin/task.push.repos.php
+++ bin/task.push.repos.php
@@ -5,7 +5,18 @@ date_default_timezone_set('America/Los_Angeles');
 
 define('DEBUG', false);
 
+openlog('task.push.repos.php', LOG_NDELAY|LOG_PID, LOG_CRON);
 list($cookie, $cookie_name) = login(WORDPRESS_USER, WORDPRESS_PASS);
+$rank_ratio = array(
+    'forks' => 0.2,
+    'stars' => 0.2,
+    'watch' => 0.2,
+    'desc'  => 0.1,
+    'push'  => 0.3,
+    'rank'  => 200000
+);
+syslog(LOG_INFO, 'now start task.push.repos.php');
+log_ratio($rank_ratio, 'start ratio');
 
 $t = time();
 $start = $t - 3600;
@@ -28,6 +39,7 @@ foreach ($pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC) as $row) {
     if ($row['watch_cnt'] > $max_watch) $max_watch = $row['watch_cnt'];
     if ($row['cnt']       > $max_cnt)   $max_cnt   = $row['cnt'];
 }
+syslog(LOG_INFO, "max_forks: $max_forks max_stars: $max_stars max_watch: $max_watch, max_push: $max_cnt");
 
 $sql = "SELECT
         `id`, `description`,
@@ -39,60 +51,83 @@ $sql = "SELECT
         WHERE `pushed` >= '$start'
         GROUP BY `id`";
 $repos = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-$filter = array();
-foreach ($repos as $row) {
-    $row['rank'] = rank($row, $max_forks, $max_stars, $max_watch, $max_cnt);
-    $row['rank'] = (int)($row['rank'] * 1000000);
-    if ($row['rank'] >= 200000) $filter[] = $row;
-}
-usort($filter, function($a, $b) {
-    return $a['rank'] < $b['rank'] ? 1 : -1;
-});
-$begin = time() - 7 * 24 * 3600;
-foreach ($filter as $row) {
-    if (!DEBUG) {
-        $sql = "SELECT COUNT(1) FROM `pushed_log` WHERE `src_id` = '${row['id']}' AND `pushed` >= '$begin'";
-        list($cnt) = $pdo->query($sql)->fetch(PDO::FETCH_NUM);
-        if ($cnt) continue;
+$published = 0;
+while ($published == 0) {
+    $filter = array();
+    foreach ($repos as $row) {
+        $row['rank'] = rank($row, $max_forks, $max_stars, $max_watch, $max_cnt, $rank_ratio);
+        $row['rank'] = (int)($row['rank'] * 1000000);
+        if ($row['rank'] >= $rank_ratio['rank']) $filter[] = $row;
     }
-    $str = output_repo($row);
-    do {
-        list($status, $header, $body) = curl_post('https://api.github.com/markdown?access_token='.GITHUB_FETCH_REPOS_TOKEN, json_encode(array(
-            'text' => $str
-        )));
-    } while ($status != 200);
-    $desc = output_description($row);
-    if (!empty($desc)) {
-        $body .= "<!--more-->\n".
-                 "<blockquote>$desc</blockquote>";
+    usort($filter, function($a, $b) {
+        return $a['rank'] < $b['rank'] ? 1 : -1;
+    });
+    $begin = time() - 7 * 24 * 3600;
+    foreach ($filter as $row) {
+        if (!DEBUG) {
+            $sql = "SELECT COUNT(1) FROM `pushed_log` WHERE `src_id` = '${row['id']}' AND `pushed` >= '$begin'";
+            list($cnt) = $pdo->query($sql)->fetch(PDO::FETCH_NUM);
+            if ($cnt) {
+                syslog(LOG_INFO, "${row['id']} has been published in this week, skiped");
+                continue;
+            }
+        }
+        $str = output_repo($row);
+        do {
+            list($status, $header, $body) = curl_post('https://api.github.com/markdown?access_token='.GITHUB_FETCH_REPOS_TOKEN, json_encode(array(
+                'text' => $str
+            )));
+        } while ($status != 200);
+        $desc = output_description($row);
+        if (!empty($desc)) {
+            $body .= "<!--more-->\n".
+                     "<blockquote>$desc</blockquote>";
+        }
+        create_post($row, $cookie, $cookie_name, $row['full_name'], $body, array($row['language']));
+        $obj = array(
+            'src_id'    => $row['id'],
+            'full_name' => $row['full_name'],
+            'language'  => $row['language'],
+            'forks_cnt' => $row['forks_cnt'],
+            'stars_cnt' => $row['stars_cnt'],
+            'watch_cnt' => $row['watch_cnt'],
+            'rank'      => $row['rank']
+        );
+        insert('pushed_log', $obj);
+        ++$published;
     }
-    create_post($cookie, $cookie_name, $row['full_name'], $body, array($row['language']));
-    $obj = array(
-        'src_id'    => $row['id'],
-        'full_name' => $row['full_name'],
-        'language'  => $row['language'],
-        'forks_cnt' => $row['forks_cnt'],
-        'stars_cnt' => $row['stars_cnt'],
-        'watch_cnt' => $row['watch_cnt'],
-        'rank'      => $row['rank']
-    );
-    insert('pushed_log', $obj);
+    if ($published == 0) {
+        rank_reduce($rank_ratio);
+        syslog(LOG_INFO, 'no repo matched');
+        log_ratio($rank_ratio, 'now ratio reduce to');
+    }
 }
+syslog(LOG_INFO, "======= all done, published $published");
 
-function rank($obj, $max_forks, $max_stars, $max_watch, $max_cnt) {
+function rank($obj, $max_forks, $max_stars, $max_watch, $max_cnt, $ratio) {
     $pfx = 0;
-    if (strlen($obj['description']) > 20) $pfx = 0.1;
-    return $obj['forks_cnt'] / $max_forks * 0.2 +
-           $obj['stars_cnt'] / $max_stars * 0.2 +
-           $obj['watch_cnt'] / $max_watch * 0.2 +
-           $obj['cnt']       / $max_cnt   * 0.3 + $pfx;
+    if (strlen($obj['description']) > 20) $pfx = $ratio['desc'];
+    return $obj['forks_cnt'] / $max_forks * $ratio['forks'] +
+           $obj['stars_cnt'] / $max_stars * $ratio['stars'] +
+           $obj['watch_cnt'] / $max_watch * $ratio['watch'] +
+           $obj['cnt']       / $max_cnt   * $ratio['push'] + $pfx;
+}
+function rank_reduce(&$ratio) {
+    $ratio['forks'] /= 2;
+    $ratio['stars'] /= 2;
+    $ratio['watch'] /= 2;
+    $ratio['desc']  /= 2;
+    $ratio['push']   = 1 - $ratio['forks'] - $ratio['stars'] - $ratio['watch'] - $ratio['desc'] - $ratio['push'];
+    $ratio['rank']  /= 2;
 }
 function output_repo(&$obj) {
     $pdo = pdo();
     $sql = "SELECT `uname`, `full_name`, `homepage`, `language`, `default_branch` FROM `repos_log` WHERE `id` = '${obj['id']}' ORDER BY `pushed` DESC LIMIT 1";
     $obj = array_merge($obj, $pdo->query($sql)->fetch(PDO::FETCH_ASSOC));
     $str = "# ${obj['full_name']}\n\n".
-           "${obj['description']}\n\n";
+           "${obj['description']}\n\n".
+           "## Link\n\n".
+           "[https://github.com/${obj['full_name']}](https://github.com/${obj['full_name']})\n\n";
     if (!empty($obj['homepage']))
         $str .= "## Homepage\n\n".
                 "[${obj['homepage']}](${obj['homepage']})\n\n";
@@ -100,7 +135,7 @@ function output_repo(&$obj) {
         $str .= "## Language\n\n".
                 "${obj['language']}\n\n";
     $str .= "## Rank\n\n".
-            "forks: **${obj['forks_cnt']}** stars: **${obj['stars_cnt']}** watch: **${obj['watch_cnt']}** rank: **${obj['rank']}**\n\n";
+            "forks: **${obj['forks_cnt']}** stars: **${obj['stars_cnt']}** watch: **${obj['watch_cnt']}** push: **${obj['cnt']}** rank: **${obj['rank']}**\n\n";
     $str .= "## Description\n\n";
     return $str;
 }
@@ -110,6 +145,7 @@ function output_description($obj) {
     } while ($status != 404 and $status != 200);
     $ret = '';
     if ($status == 200) {
+        if (empty($body)) return '';
         $content = $body;
         do {
             list($status, $header, $ret) = curl_post('https://api.github.com/markdown?access_token='.GITHUB_FETCH_REPOS_TOKEN, json_encode(array(
@@ -144,7 +180,7 @@ function categories_convert($cats) {
     } unset($cat);
     return $cats;
 }
-function create_post($cookie, $cookie_name, $title, $content, $categories) {
+function create_post($obj, $cookie, $cookie_name, $title, $content, $categories) {
     list($status, $header, $body) = curl_get(WORDPRESS_HOST."/?json=get_nonce&controller=posts&method=create_post&cookie=$cookie", array($cookie_name.'='.$cookie));
     if ($status != 200) return false;
     $body = json_decode($body, true);
@@ -160,13 +196,24 @@ function create_post($cookie, $cookie_name, $title, $content, $categories) {
     if (count($categories))
         $post['categories'] = urlencode(implode(',', categories_convert($categories)));
     list($status, $header, $body) = curl_post(WORDPRESS_HOST, $post, array($cookie_name.'='.$cookie));
-    if ($status != 200) return false;
+    if ($status != 200) {
+        syslog(LOG_ERR, "${obj['full_name']} create_post failed");
+        return false;
+    }
     $body = json_decode($body, true);
     $id = $body['post']['id'];
     $cats = array_column($body['post']['categories'], 'title');
-    if (count($categories) and count(array_diff($categories, $cats))) # have uncatched category
+    if (count($categories) and count(array_diff($categories, $cats))) { # have uncatched category
+        syslog(LOG_INFO, "${obj['full_name']} has uncatched category, no published");
         return true;
-    return publish_post($cookie, $cookie_name, $id);
+    }
+    if (publish_post($cookie, $cookie_name, $id)) {
+        syslog(LOG_INFO, "${obj['full_name']} has been published, ${obj['forks_cnt']}[forks] ${obj['stars_cnt']}[stars] ${obj['watch_cnt']}[watch] ${obj['cnt']}[push] ${obj['rank']}[rank]");
+        return true;
+    } else {
+        syslog(LOG_ERR, "${obj['full_name']} publish failed");
+        return false;
+    }
 }
 function publish_post($cookie, $cookie_name, $id) {
     list($status, $header, $body) = curl_get(WORDPRESS_HOST."/?json=get_nonce&controller=posts&method=update_post&cookie=$cookie", array($cookie_name.'='.$cookie));
@@ -181,4 +228,7 @@ function publish_post($cookie, $cookie_name, $id) {
         'status' => 'publish'
     ), array($cookie_name.'='.$cookie));
     return $status == 200;
+}
+function log_ratio($ratio, $pfx) {
+    syslog(LOG_INFO, "$pfx: ${ratio['forks']}[forks] ${ratio['stars']}[stars] ${ratio['watch']}[watch] ${ratio['desc']}[desc] ${ratio['push']}[push] ${ratio['rank']}[rank]");
 }
