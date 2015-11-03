4f91c4f81e96f7812f333e32f8eca2138aad3ab4
lwch <lwch748@gmail.com>
2015-11-03 16:41:56 +0800

    backup wp_terms

--- sql/wp_terms.sql
+++ sql/wp_terms.sql
@@ -30,7 +30,7 @@ CREATE TABLE `wp_terms` (
   PRIMARY KEY (`term_id`),
   KEY `slug` (`slug`(191)),
   KEY `name` (`name`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `wp_terms` (
 
 LOCK TABLES `wp_terms` WRITE;
 /*!40000 ALTER TABLE `wp_terms` DISABLE KEYS */;
-INSERT INTO `wp_terms` VALUES (1,'other','other',0),(6,'C','c',0),(9,'main','main',0),(12,'Java','java',0),(13,'C++','c-2',0),(14,'JavaScript','javascript',0),(15,'HTML','html',0),(16,'Python','python',0),(17,'CoffeeScript','coffeescript',0),(18,'Go','go',0),(19,'Ruby','ruby',0),(20,'CSS','css',0),(21,'Objective-C','objective-c',0),(22,'C#','c-3',0),(23,'Shell','shell',0),(24,'PHP','php',0),(25,'Scala','scala',0),(26,'VimL','viml',0),(27,'Julia','julia',0),(28,'Groff','groff',0),(29,'Rust','rust',0),(30,'Clojure','clojure',0),(31,'Perl','perl',0),(32,'TeX','tex',0),(33,'Puppet','puppet',0),(34,'PLSQL','plsql',0),(35,'Swift','swift',0),(36,'Forth','forth',0),(37,'Batchfile','batchfile',0),(38,'Nix','nix',0),(39,'Elixir','elixir',0),(40,'Perl6','perl6',0),(41,'TypeScript','typescript',0),(42,'Lua','lua',0),(43,'R','r',0),(44,'Smarty','smarty',0),(45,'Scheme','scheme',0),(46,'Dart','dart',0),(47,'KiCad','kicad',0),(48,'Liquid','liquid',0),(49,'Haskell','haskell',0),(50,'Eagle','eagle',0),(51,'F#','f',0),(52,'Yacc','yacc',0),(53,'ASP','asp',0),(54,'D','d',0),(55,'OCaml','ocaml',0),(56,'Kotlin','kotlin',0),(57,'SQF','sqf',0),(58,'Makefile','makefile',0),(59,'Matlab','matlab',0),(60,'Visual Basic','visual-basic',0),(61,'Chapel','chapel',0),(62,'Tcl','tcl',0),(63,'Mathematica','mathematica',0),(64,'Max','max',0),(65,'Processing','processing',0),(66,'Racket','racket',0),(67,'Arduino','arduino',0),(68,'PLpgSQL','plpgsql',0),(69,'Pascal','pascal',0),(70,'Hack','hack',0),(71,'Groovy','groovy',0),(72,'ActionScript','actionscript',0),(73,'CMake','cmake',0),(74,'Prolog','prolog',0),(75,'Turing','turing',0),(76,'Common Lisp','common-lisp',0),(77,'SourcePawn','sourcepawn',0),(78,'AutoHotkey','autohotkey',0),(79,'Erlang','erlang',0),(80,'PowerShell','powershell',0),(81,'AMPL','ampl',0),(82,'Crystal','crystal',0);
+INSERT INTO `wp_terms` VALUES (1,'other','other',0),(6,'C','c',0),(9,'main','main',0),(12,'Java','java',0),(13,'C++','c-2',0),(14,'JavaScript','javascript',0),(15,'HTML','html',0),(16,'Python','python',0),(17,'CoffeeScript','coffeescript',0),(18,'Go','go',0),(19,'Ruby','ruby',0),(20,'CSS','css',0),(21,'Objective-C','objective-c',0),(22,'C#','c-3',0),(23,'Shell','shell',0),(24,'PHP','php',0),(25,'Scala','scala',0),(26,'VimL','viml',0),(27,'Julia','julia',0),(28,'Groff','groff',0),(29,'Rust','rust',0),(30,'Clojure','clojure',0),(31,'Perl','perl',0),(32,'TeX','tex',0),(33,'Puppet','puppet',0),(34,'PLSQL','plsql',0),(35,'Swift','swift',0),(36,'Forth','forth',0),(37,'Batchfile','batchfile',0),(38,'Nix','nix',0),(39,'Elixir','elixir',0),(40,'Perl6','perl6',0),(41,'TypeScript','typescript',0),(42,'Lua','lua',0),(43,'R','r',0),(44,'Smarty','smarty',0),(45,'Scheme','scheme',0),(46,'Dart','dart',0),(47,'KiCad','kicad',0),(48,'Liquid','liquid',0),(49,'Haskell','haskell',0),(50,'Eagle','eagle',0),(51,'F#','f',0),(52,'Yacc','yacc',0),(53,'ASP','asp',0),(54,'D','d',0),(55,'OCaml','ocaml',0),(56,'Kotlin','kotlin',0),(57,'SQF','sqf',0),(58,'Makefile','makefile',0),(59,'Matlab','matlab',0),(60,'Visual Basic','visual-basic',0),(61,'Chapel','chapel',0),(62,'Tcl','tcl',0),(63,'Mathematica','mathematica',0),(64,'Max','max',0),(65,'Processing','processing',0),(66,'Racket','racket',0),(67,'Arduino','arduino',0),(68,'PLpgSQL','plpgsql',0),(69,'Pascal','pascal',0),(70,'Hack','hack',0),(71,'Groovy','groovy',0),(72,'ActionScript','actionscript',0),(73,'CMake','cmake',0),(74,'Prolog','prolog',0),(75,'Turing','turing',0),(76,'Common Lisp','common-lisp',0),(77,'SourcePawn','sourcepawn',0),(78,'AutoHotkey','autohotkey',0),(79,'Erlang','erlang',0),(80,'PowerShell','powershell',0),(81,'AMPL','ampl',0),(82,'Crystal','crystal',0),(83,'PostScript','postscript',0),(84,'FORTRAN','fortran',0),(85,'XSLT','xslt',0),(86,'Emacs Lisp','emacs-lisp',0),(87,'Assembly','assembly',0),(88,'API Blueprint','api-blueprint',0),(89,'Standard ML','standard-ml',0),(90,'VHDL','vhdl',0),(91,'Haxe','haxe',0),(92,'Grace','grace',0);
 /*!40000 ALTER TABLE `wp_terms` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-03  9:49:43
+-- Dump completed on 2015-11-03 16:41:44
